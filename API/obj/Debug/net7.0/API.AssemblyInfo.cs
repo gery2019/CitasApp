453b68206e8cb6a679e095e5a3f6252259ca8804
Gerys_124 <gerardolopezvs@outlook.com>
2024-03-11 07:08:30 -0600

    Added feedback message to validators

--- API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8f6e7bf242568c466550a6188b61979650b0289")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce3bce2a6589aaeb7a5237142129ed6005d22738")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- SPA/src/app/register/register.component.ts
+++ SPA/src/app/register/register.component.ts
@@ -1,54 +1,59 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
-import { AbstractControl, Form, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model: any= {};
+  model: any = {};
   registerForm: FormGroup = new FormGroup({});
 
-  constructor(private accountService: AccountService, private toastr: ToastrService){};
+  constructor(private accountService: AccountService, private toastr: ToastrService) { }
 
-  NgOnInit(): void{
+  ngOnInit(): void {
     this.initializeForm();
   }
 
-  initializeForm(){
+  initializeForm() {
     this.registerForm = new FormGroup({
       username: new FormControl("", Validators.required),
       password: new FormControl("", [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl("", [Validators.required, this.matchvalues('password')]),
-    })
-    this.registerForm.controls['password'].valueChanges.subscribe({
-      next: () => this.registerForm.controls['confirmPassword'].updateValueAndValidity()
+      confirmPassword: new FormControl("", [Validators.required,
+      this.matchValues("password")]),
+    });
+    
+    this.registerForm.controls["password"].valueChanges.subscribe({
+      next: () => this.registerForm.controls["confirmPassword"].updateValueAndValidity(),
     });
   }
 
-  matchvalues(matchTo: string): ValidatorFn{
+  matchValues(matchTo: string): ValidatorFn {
     return (control: AbstractControl) => {
-      return control.value === control.parent?.get(matchTo)?.value ? null : {notMatching: true};
+      return control.value === control.parent?.get(matchTo)?.value
+        ? null : { notMatching: true };
     }
+  }
 
+  register(): void {
+    console.log(this.registerForm?.value);
+    // this.accountService.register(this.model).subscribe({
+    //   next: () => {
+    //     this.cancel();
+    //   },
+    //   error: error => {
+    //     this.toastr.error(error.error);
+    //     console.log(error);
+    //   }
+    // });
   }
-  register(): void{
-    this.accountService.register(this.model).subscribe({
-      next: () => {
-        this.cancel();
-      },
-      error: error => {
-        this.toastr.error(error.error),
-        console.log(error)
-      }
-    });    
-  }
-  cancel(): void{
-    this.cancelRegister.emit(false);    
+
+  cancel(): void {
+    this.cancelRegister.emit(false);
   }
 
 }
