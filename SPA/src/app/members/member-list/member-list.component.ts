ea069accd757495bc8f165062f00e962d4d2a1f3
Gerys_124 <gerardolopezvs@outlook.com>
2024-03-25 20:47:30 -0600

    Setup Pagination in SPA

--- SPA/src/app/members/member-list/member-list.component.ts
+++ SPA/src/app/members/member-list/member-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IMember } from 'src/app/_models/imember';
+import { Pagination } from 'src/app/_models/pagination';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
@@ -9,11 +10,27 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-list.component.css'],
 })
 export class MemberListComponent implements OnInit {
-  members$: Observable<IMember[]>  | undefined;
+  // members$: Observable<IMember[]>  | undefined;
+  members: IMember[] = [];
+  pagination: Pagination | undefined;
+  pageNumber = 1;
+  pageSize = 5;
+  
 
   constructor(private membersService: MembersService) {}
 
   ngOnInit(): void {
-    this.members$ = this.membersService.getMembers();
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.membersService.getMembers(this.pageNumber, this.pageSize).subscribe({
+      next: response => {
+        if (response.result && response.pagination) {
+          this.members = response.result;
+          this.pagination = response.pagination;
+        }
+      }
+    })
   }
 }
